08c6b4252ad235c603ef1287326ca32c02fb03de
tomas <tomas@tomas-X555LAB>
2018-11-20 20:55:39 -0300

    Se añadio los fixtures 2 y 3 del modelo, y se crearon las clases de prueba correspondientes

--- src/ModeloTest/ModeloTestSuite.java
+++ src/ModeloTest/ModeloTestSuite.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ModeloTest1.class})
+@Suite.SuiteClasses({ModeloTest1.class,ModeloTest2.class,ModeloTest3.class})
 public class ModeloTestSuite {
 
 }
